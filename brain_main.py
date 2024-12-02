43ffdf5b87fb4a035cacc5df361b9325cad2a5b8
Jan Solé <jan@sole.cat>
2024-12-02 16:32:41 +0100

    sift added

--- brain_main.py
+++ brain_main.py
@@ -2,6 +2,9 @@ import brain_utils as bu
 import os
 import cv2
 import sklearn
+import time
+
+t0 = time.time()
 
 ### Primer processament de les dades, resize a 256x256 i ordenament a les seves respectives llistes
 dir = './Brain Cancer'
@@ -22,20 +25,25 @@ for cancer_type in os.listdir(dir):                             # pels 3 tipus d
         images[cancer_type].append(img_gray)
         # print(images)
 
+t1 = time.time()
+print('Imatges processades en:', t1-t0)
 ### Ara ja tenim les imatges processades i guardades en el diccionari d'imatges
 # SIFT:
 
-sift = cv2.SIFT_create()
-
-keypoints_descriptors = {}
+dense_sift_features = {}
+step_size = 8  # Ajusta la densitat segons calgui
 
 for cancer_type, img_list in images.items():
-    keypoints_descriptors[cancer_type] = []
+    dense_sift_features[cancer_type] = []
     for img in img_list:
-        keypoints, descriptors = sift.detectAndCompute(img, None)
-        keypoints_descriptors[cancer_type].append(descriptors)
+        print('ei')
+        keypoints, descriptors = bu.denseSIFT_mask(img, step_size=step_size) # te una mascara binaria, ignora tots els pixels que estiguin a 0
+        dense_sift_features[cancer_type].append(descriptors)  # Guarda descriptors
 
-print(keypoints_descriptors)
+print(dense_sift_features)
+
+t2 = time.time()
+print('SIFT descriptors creats en:', t2-t1)
 
 ### BoW - kmeans?
 
@@ -43,5 +51,5 @@ print(keypoints_descriptors)
 ### histograma, X,y...
 
 
-### SVC? no se si es viable amb tantes dimensions.1
+### SVC? no se si es viable amb tantes dimensions. --> rbf
 
--- brain_utils.py
+++ brain_utils.py
@@ -7,11 +7,45 @@ import sklearn
 
 def resize_image(image_path, target_size=(256, 256)):
 
-    """Funció de PSIV de tota la vida, agafem l'imatge i redimensionem +BW per si de cas"""
+    """
+    Funció de PSIV de tota la vida, agafem l'imatge i redimensionem +BW per si de cas
+    
+    :param image_path: cami de l'imatge
+    :param target_size: mida final de la foto
+    
+    """
     img = cv2.imread(image_path)
     img_resized = cv2.resize(img, target_size)
     img_gray = cv2.cvtColor(img_resized, cv2.COLOR_BGR2GRAY)
 
     return img_gray
+
+
+def denseSIFT_mask(image, step_size=8):
+    """
+    Aplica Dense SIFT sobre una imatge usant una mascara +1 per ignorar zones no rellevants
+
+    :param image: Imatge (numpy array) en escala de grisos.
+    :param step_size: Separació entre punts clau (int).
+    :return: Punts clau (keypoints) i descriptors SIFT.
+    """
+    # Crea la màscara automàticament (ignora el negre)
+    _, mask = cv2.threshold(image, 1, 255, cv2.THRESH_BINARY)  # Detecta píxels no negres (1 o més)
+
+    # Defineix els punts clau en graella
+    keypoints = [
+        cv2.KeyPoint(x, y, step_size)
+        for y in range(0, image.shape[0], step_size)
+        for x in range(0, image.shape[1], step_size)
+        if mask[y, x] > 0  # Inclou només punts dins de la màscara
+    ]
+
+    # Crea l'objecte SIFT
+    sift = cv2.SIFT_create()
+
+    # Calcula descriptors pels punts clau dins de la màscara
+    keypoints, descriptors = sift.compute(image, keypoints)
+    return keypoints, descriptors
+
 
 

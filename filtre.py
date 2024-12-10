cb5026af98b834f401c4c7b263e551d3664f7100
quimsolerrovira <114931456+quimsolerrovira@users.noreply.github.com>
2024-12-10 16:21:21 +0100

    Update filtre.py

--- filtre.py
+++ filtre.py
@@ -14,7 +14,7 @@ def is_top_view(image, margin=10, threshold=50):
         return True
     return False
 
-def filter_images(input_folder, output_folder, num_images, margin=10, threshold=50):
+def filter_images(input_folder, output_folder, num_images=-1, margin=10, threshold=50):
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
 
@@ -45,8 +45,20 @@ def filter_images(input_folder, output_folder, num_images, margin=10, threshold=
             filtered_count += 1
 
     print(f"Number of images filtered from {input_folder}: {filtered_count}")
+    return filtered_count
 
-def filter_images_by_type(input_folders, output_base_folder, num_images_per_type, margin=10, threshold=50):
+def filter_images_by_type(input_folders, output_base_folder, margin=10, threshold=50):
+    # Filter all images from the 'notumor' folder first
+    notumor_filtered_count = filter_images(input_folders["sans"], os.path.join(output_base_folder, "notumor_filtrat"), margin=margin, threshold=threshold)
+
+    # Calculate the number of images to filter for each type based on the number of 'notumor' images filtered
+    total_filtered_images = notumor_filtered_count / 0.50
+    num_images_per_type = {
+        "meningioma": int(total_filtered_images * 0.225),
+        "glioma": int(total_filtered_images * 0.175),
+        "pituitari": int(total_filtered_images * 0.10)
+    }
+
     for folder_type, num_images in num_images_per_type.items():
         input_folder = input_folders[folder_type]
         output_folder = os.path.join(output_base_folder, f"{folder_type}_filtrat")
@@ -59,19 +71,11 @@ input_folders = {
     "glioma": "Brain Cancer/glioma",
     "pituitari": "Brain Cancer/pituitary"
 }
-output_base_folder = "Brain Cancer"
-
-# Percentages and number of images per type
-num_images_per_type = {
-    "sans": 2000,
-    "meningioma": 900,
-    "glioma": 700,
-    "pituitari": 400
-}
+output_base_folder = "Brain Cancer/filtre"
 
 for folder in input_folders.values():
     if not os.path.exists(folder):
         print(f"Error: The input folder '{folder}' does not exist.")
         break
 else:
-    filter_images_by_type(input_folders, output_base_folder, num_images_per_type, margin=15, threshold=50)
+    filter_images_by_type(input_folders, output_base_folder, margin=15, threshold=50)

52dcb05a2aa19c9c8c86ec70a13e97a320dc8d23
quimsolerrovira <114931456+quimsolerrovira@users.noreply.github.com>
2024-12-16 19:23:27 +0100

    prop filtre.py
    hem separat la funció d'aplicar les proporcions

--- filtre.py
+++ filtre.py
@@ -56,8 +56,16 @@ def filter_images(input_folder, output_folder, not_filtered_folder, num_images=-
     return filtered_count
 
 def filter_images_by_type(input_folders, output_base_folder, not_filtered_folder, margin=10, threshold=50):
-    notumor_filtered_count = filter_images(input_folders["sans"], os.path.join(output_base_folder, "notumor"), not_filtered_folder, margin=margin, threshold=threshold) #en cas de voler afegir 'filtrat' pels sans, es posa aqui
+    filter_images(input_folders["sans"], os.path.join(output_base_folder, "notumor"), not_filtered_folder, margin=margin, threshold=threshold)
 
+    for folder_type in ["meningioma", "glioma", "pituitary"]:
+        input_folder = input_folders[folder_type]
+        output_folder = os.path.join(output_base_folder, f"{folder_type}")
+        filter_images(input_folder, output_folder, not_filtered_folder, margin=margin, threshold=threshold)
+
+def apply_proportions(input_folders, output_base_folder):
+    notumor_filtered_count = len(os.listdir(os.path.join(output_base_folder, "notumor")))
+    
     total_filtered_images = notumor_filtered_count / 0.50
     num_images_per_type = {
         "meningioma": int(total_filtered_images * 0.225),
@@ -65,10 +73,27 @@ def filter_images_by_type(input_folders, output_base_folder, not_filtered_folder
         "pituitary": int(total_filtered_images * 0.10)
     }
 
+    proportions_folder = os.path.join(output_base_folder, "proporcions")
+    
     for folder_type, num_images in num_images_per_type.items():
-        input_folder = input_folders[folder_type]
-        output_folder = os.path.join(output_base_folder, f"{folder_type}") #en cas de voler afegir 'filtrat' pels malalts, es posa aqui
-        filter_images(input_folder, output_folder, not_filtered_folder, num_images, margin, threshold)
+        input_folder = os.path.join(output_base_folder, folder_type)
+        output_folder = os.path.join(proportions_folder, folder_type)
+        
+        if not os.path.exists(output_folder):
+            os.makedirs(output_folder)
+        
+        images = [f for f in os.listdir(input_folder) if f.endswith(('.png', '.jpg', '.jpeg'))]
+        random.shuffle(images)  # Shuffle images to get a random selection
+        
+        for i in range(min(num_images, len(images))):
+            image_path = os.path.join(input_folder, images[i])
+            output_path = os.path.join(output_folder, images[i])
+            try:
+                image = imageio.imread(image_path)
+                imageio.imwrite(output_path, image)
+                print(f"Image saved to: {output_path}")
+            except Exception as e:
+                print(f"Error processing the image {images[i]}: {e}")
 
 def dos_tres(input_folder):
     images = [f for f in os.listdir(input_folder) if f.endswith(('.png', '.jpg', '.jpeg'))]
@@ -76,7 +101,7 @@ def dos_tres(input_folder):
     for image in images:
         image_path = os.path.join(input_folder, image)
         img = np.array(cv2.imread(image_path))
-        if image is not None:
+        if img is not None:
             tall = (2 * img.shape[0])//3
             img[tall:,:] = 0
 
@@ -98,3 +123,8 @@ for folder in input_folders.values():
         break
 else:
     filter_images_by_type(input_folders, output_base_folder, not_filtered_folder, margin=12, threshold=50)
+
+#apliquem les proporcions a partir de la carpeta de pacients sans (carpeta ja filtrada)
+apply_proportions(input_folders, output_base_folder)
+
+
